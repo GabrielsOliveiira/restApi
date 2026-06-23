f5a964cfb14ec2fd3103d6d3cae3a37e6b739dca
Gabriel <gabriel.s.oliveiira@gmail.com>
2026-06-23 16:53:01 -0300

    validando senha

--- main/services/auth_service.py
+++ main/services/auth_service.py
@@ -1,9 +1,15 @@
 from main.models import User
+from werkzeug.security import check_password_hash
 
 def login(data):
     user = User.query.filter_by(email=data["email"]).first()
 
     if not user:
+        return {
+            "error": "Email ou senha inválidos"
+        }, 401
+
+    if not check_password_hash(user.password, data["senha"]):
         return {
             "error": "Email ou senha inválidos"
         }, 401
