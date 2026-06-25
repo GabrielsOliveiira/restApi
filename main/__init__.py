a835df8019f203ae53ae2eb2d75e1668cb3c569e
Gabriel <gabriel.s.oliveiira@gmail.com>
2026-06-25 19:56:52 -0300

    Expoertando excel

--- main/__init__.py
+++ main/__init__.py
@@ -18,6 +18,7 @@ def create_app():
     from .routes.dashboard import dashboard_bp
     from .routes.goal import goal_bp
     from .routes.export_csv import csv_blueprint
+    from .routes.export_excel import excel_bp
     from . import models
 
     app.register_blueprint(users_bp)
@@ -26,5 +27,6 @@ def create_app():
     app.register_blueprint(goal_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(csv_blueprint)
+    app.register_blueprint(excel_bp)
 
     return app
