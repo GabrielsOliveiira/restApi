c43e81e03136b57bf348ae52fadf44ea7400713a
Gabriel <gabriel.s.oliveiira@gmail.com>
2026-06-27 13:32:53 -0300

    adicionando front no docker

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,22 +1,36 @@
-async function buscarUsuarios() {
-    const response = await fetch("http://127.0.0.1:5000/users", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
+// async function buscarUsuarios() {
+//     const response = await fetch("http://127.0.0.1:5000/users", {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
 
-    const usuarios = await response.json();
+//     const usuarios = await response.json();
 
-    console.log(usuarios);
-    console.log(usuarios[0]);
-    console.log(usuarios[0]["name"]);
+//     console.log(usuarios);
+//     console.log(usuarios[0]);
+//     console.log(usuarios[0]["name"]);
 
-    const nome = document.getElementById("nome");
+//     const nome = document.getElementById("nome");
     
-    nome.textContent = usuarios[0]["name"]
+//     nome.textContent = usuarios[0]["name"]
 
-    return usuarios
-}
+//     return usuarios
+// }
 
-buscarUsuarios();
+// buscarUsuarios();
+
+
+const email = document.getElementById("email");
+
+const senha = document.getElementById("senha")
+
+const btnSubmit = document.getElementById("btnSubmit")
+
+btnSubmit.addEventListener("click", (event) =>{
+    event.preventDefault();
+    console.log(email.value, senha.value)
+})
+
+
